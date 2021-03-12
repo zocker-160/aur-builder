f17028295c2be591dad54ac4e33a1d1219a49d6f
zocker-160 <zocker1600@posteo.net>
2021-03-12 20:27:50 +0100

    add the local buid script

---
+++ zlocal_hub.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+if [ -z "$1" ];
+then
+	echo "pls specify a software name!";
+	exit;
+fi
+
+echo "Building $1 for archlinux...."
+
+docker run --name builder --user builder --net host zocker160/aur-builder:manjaro "$1"
+docker cp builder:/results .
+docker rm builder
+
+echo "DONE! - look into the results folder for the package files"
